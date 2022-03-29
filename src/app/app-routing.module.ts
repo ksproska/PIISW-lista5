a194b25072e887aba4daf2fdcaee1e256a047156
Maciej Malecki <maciej.malecki@capgemini.com>
2022-03-29 22:45:25 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from "./books/components/book-list/book-list.component";
+import { BookListResolver } from "./books/resolvers/book-list.resolver";
+
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BookListComponent,
+    resolve: {
+      books: BookListResolver
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
